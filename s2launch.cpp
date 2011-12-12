c3fdb7af4b6450d1cad0479f073794ffcff3acc8
Tung Nguyen <tunginobi@gmail.com>
2011-12-12 16:31:31 +1100

    Disable play btn while engine is running in Linux

--- s2launch.cpp
+++ s2launch.cpp
@@ -1,6 +1,15 @@
 #ifdef _WIN32
-    #include <nall/windows/utf8.hpp>
-    void waitCallback(void *, unsigned char);
+#include <nall/windows/utf8.hpp>
+void waitCallback(void *, unsigned char);
+#else
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+void handleSigChld(int);
+void ignoreSigChld();
+struct sigaction sa;
 #endif
 
 #include <phoenix/phoenix.hpp>
@@ -41,7 +50,7 @@ struct Application : Window {
 
     void finishWait() {
         engineRunning = false;
-        btnPlay.setEnabled(lstGames.selected() && linEngine.text() != "" && !engineRunning);
+        btnPlay.setEnabled(lstGames.selected() && linEngine.text() != "");
     }
 
     void cleanWaitObject() {
@@ -92,14 +101,45 @@ struct Application : Window {
     #else
     // *** BEGIN NOT WINDOWS ***
 
-    void reallyRunEngine(const string& engine, const string& game, const string& script) {
-        string args = {
-            "\"", engine, "\"",
-            " -data \"", game, "\"",
-            " -main \"", script, "\""
-        };
+    void finishEngine() {
+        engineRunning = false;
+        btnPlay.setEnabled(lstGames.selected() && linEngine.text() != "");
+    }
 
-        MessageWindow::information(*this, { engine, "\n", args });
+    void reallyRunEngine(const string& engine, const string& game, const string& script) {
+        // Just fork and return if this is the parent process.
+        // The rest of this function is for the child process only.
+        pid_t pid = fork();
+        if (pid == -1) MessageWindow::critical(*this, "Could not run engine");
+        if (pid > 0) {
+            engineRunning = true;
+            btnPlay.setEnabled(false);
+        }
+        if (pid != 0) return;
+
+        // C strings are needed for execv.
+        char cStrEngine[engine.length() + 1];
+        char cStrData[] = "-data";
+        char cStrGame[game.length() + 1];
+        char cStrMain[] = "-main";
+        char cStrScript[script.length() + 1];
+        unsigned n;
+        for (n = 0; engine[n]; n++) cStrEngine[n] = engine[n];
+        cStrEngine[n] = '\0';
+        for (n = 0; game[n]; n++) cStrGame[n] = game[n];
+        cStrGame[n] = '\0';
+        for (n = 0; script[n]; n++) cStrScript[n] = script[n];
+        cStrScript[n] = '\0';
+
+        char *args[6];
+        args[0] = cStrEngine;
+        args[1] = cStrData;
+        args[2] = cStrGame;
+        args[3] = cStrMain;
+        args[4] = cStrScript;
+        args[5] = nullptr;
+
+        if (execv(cStrEngine, args)) exit(1);
     }
 
     // *** END NOT WINDOWS ***
@@ -227,6 +267,8 @@ struct Application : Window {
         onClose = [this]() {
             #ifdef _WIN32
             cleanWaitObject();
+            #else
+            ignoreSigChld();
             #endif
             if (configModified) writeConfig();
             OS::quit();
@@ -286,9 +328,27 @@ struct Application : Window {
 void waitCallback(void *param, unsigned char timedOut) {
     application.finishWait();
 }
+#else
+void handleSigChld(int signal) {
+    application.finishEngine();
+    wait(nullptr);
+}
+
+void listenSigChld() {
+    sa.sa_handler = handleSigChld;
+    sigaction(SIGCHLD, &sa, NULL);
+}
+
+void ignoreSigChld() {
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGCHLD, &sa, NULL);
+}
 #endif
 
 int main() {
+    #ifndef _WIN32
+    listenSigChld();
+    #endif
     application.create();
     OS::main();
     return 0;
